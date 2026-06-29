044c6c2ccca95b3c48749e506c2ac9e3925eddff
mugekbs <mugeasena@gmail.com>
2026-06-29 15:05:03 +0300

    Add app entry point

---
+++ SudokuApp/SudokuAppApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct SudokuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.light)
+        }
+    }
+}
